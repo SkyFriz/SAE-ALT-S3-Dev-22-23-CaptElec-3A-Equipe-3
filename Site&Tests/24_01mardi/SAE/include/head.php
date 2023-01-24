0c4e75bcaf5219ac901ae5bdc2f482dd93635a3f
Thomas Testa <thomastesta@orange.fr>
2023-01-24 17:05:41 +0100

    fin de la journée

--- Site&Tests/24_01mardi/SAE/include/head.php
+++ Site&Tests/24_01mardi/SAE/include/head.php
@@ -4,24 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ouai</title>
-    <link rel="stylesheet" href="styleInclude.css">
+    <link rel="stylesheet" href="styleInclnclude.css">
   </head>
   <body>
     <nav class="navbar">
-        <a href="#" class="logo">ADVENTURE</a>
+        <a href="#" class="logo">CaptElec</a>
         <div class="nav-links">
             <ul>
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Adventure</a></li>
-                <li><a href="#">Support</a></li>
-                <li><a href="#">Info</a></li>
+                <li class="active"><a href="#">Accueil</a></li>
+                <li><a href="#">Rechercher</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
         </div>
         <img src="img/menu-btn.png" alt="menu hamburger" class="menu-hamburger">
     </nav>
   </body>
+  <script>
+        const menuHamburger = document.querySelector(".menu-hamburger")
+        const navLinks = document.querySelector(".nav-links")
+ 
+        menuHamburger.addEventListener('click',()=>{
+        navLinks.classList.toggle('mobile-menu')
+        })
+  </script>
 </html>
 
 
